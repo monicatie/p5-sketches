02c30be685f3bbbded3fcc9ce544dc499283ca8a
Monica Tie <monica.y.tie@gmail.com>
2021-05-01 10:30:07 -0600

    Improves on blendbars and moves them to their own lib

--- sketch-2/sketch.js
+++ sketch-2/sketch.js
@@ -1,13 +1,17 @@
 let img1; // Declare variable 'img'.
 let img2;
 var capturer;
+let canvas = {
+  width: 500,
+  height: 500,
+}
 
 function setup() {
-  frameRate(15);
-  createCanvas(500, 500);
+  frameRate(1);
+  createCanvas(canvas.width, canvas.height);
   capturer = new window.CCapture({
     format: 'webm',
-    framerate: 15
+    framerate: 6
   });
   img1 = loadImage('/assets/DSC03305.jpg'); // Load the image
   img2 = loadImage('/assets/DSC03308.jpg'); // Load the image
@@ -17,19 +21,17 @@ function draw() {
   // record(3000, () => {
     background(img1);
     image(img2, 0, 0);
-    let x = random(500)
-    let y = random(500)
+    let x = random(50, canvas.width-100)
+    let y = random(50, canvas.height-100)
     let width = random(30, 250);
     let height = random(30, 250);
 
-    let barWidth = 5;
+    let barWidth = 2;
     let spacing = 2;
-    // if (width > height) {
-      // for (var i = 0; i < Math.floor(height/(barWidth+spacing)); i++) {
-      //   blend(img1, x, y, width, barWidth, x, y, width, barWidth, SUBTRACT);
-      // }
-    // }
-  // })
-    blend(img1, 100, 100, 50, 200, 100, 100, 50, 200, SUBTRACT);
-
+    if (width < height) {
+      blendVerticalBars(x, y, width, height, barWidth, spacing, 12, DARKEST);
+    } else {
+      blendHorizontalBars(x, y, width, height, barWidth, spacing, 20, DARKEST)
+    }
+  // });
 }
