b45f83fa1ad43d0cb254e34616b8ab4e3ab5071e
Monica Tie <monica.y.tie@gmail.com>
2021-05-01 13:53:50 -0600

    Turns off record for second sketch

--- sketch-2/sketch.js
+++ sketch-2/sketch.js
@@ -7,31 +7,42 @@ let canvas = {
 }
 
 function setup() {
-  frameRate(1);
+  frameRate(8);
   createCanvas(canvas.width, canvas.height);
   capturer = new window.CCapture({
     format: 'webm',
-    framerate: 6
+    framerate: 8
   });
   img1 = loadImage('/assets/DSC03305.jpg'); // Load the image
   img2 = loadImage('/assets/DSC03308.jpg'); // Load the image
 }
 
 function draw() {
-  // record(3000, () => {
+  // record(8000, () => {
     background(img1);
     image(img2, 0, 0);
-    let x = random(50, canvas.width-100)
-    let y = random(50, canvas.height-100)
-    let width = random(30, 250);
-    let height = random(30, 250);
-
-    let barWidth = 2;
-    let spacing = 2;
-    if (width < height) {
-      blendVerticalBars(x, y, width, height, barWidth, spacing, 12, DARKEST);
-    } else {
-      blendHorizontalBars(x, y, width, height, barWidth, spacing, 20, DARKEST)
-    }
+    const maxWidth = 100;
+    const variation = 20;
+    
+    drawBlendSection(2, 2, variation, MULTIPLY, maxWidth);
+    drawBlendSection(2, 2, variation, MULTIPLY, maxWidth);
+    drawBlendSection(2, 2, variation, LIGHTEST, maxWidth);
+    drawBlendSection(2, 2, variation, LIGHTEST, maxWidth);
+    drawBlendSection(2, 2, variation, SUBTRACT, maxWidth);
+    drawBlendSection(2, 2, variation, BURN, maxWidth);
+    drawBlendSection(2, 2, variation, BURN, maxWidth);
   // });
+}
+
+function drawBlendSection(barWidth, spacing, variation, blendMode, maxWidth ) {
+  let x = random(50, canvas.width-100)
+  let y = random(50, canvas.height-100)
+  let width = random(30, maxWidth);
+  let height = random(30, maxWidth);
+
+  if (width < height) {
+    blendVerticalBars(x, y, width, height, barWidth, spacing, variation, blendMode);
+  } else {
+    blendHorizontalBars(x, y, width, height, barWidth, spacing, variation, blendMode)
+  }
 }
